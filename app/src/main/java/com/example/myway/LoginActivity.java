af9abcf61f8684714cf065998e1face30067c514
yeo-yiheng <yeoyiheng0211@gmail.com>
2021-05-27 17:04:24 +0800

    Added password visibility toggle option Added a register button and empty register activity

--- app/src/main/java/com/example/myway/LoginActivity.java
+++ app/src/main/java/com/example/myway/LoginActivity.java
@@ -4,26 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+@SuppressWarnings("deprecation")
 public class LoginActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button loginButton;
+    private Button registerButton;
+    private ImageButton visibilityButton;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        button = findViewById(R.id.Login);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(LoginActivity.this, "Logging In...", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+        loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(v -> {
+            Toast.makeText(LoginActivity.this, "Logging In...", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        registerButton = findViewById(R.id.register);
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        visibilityButton = findViewById(R.id.visibility_button);
+        visibilityButton.setOnClickListener(v -> {
+            password = findViewById(R.id.password);
+            Toast toast;
+            if (visibilityButton.getContentDescription().equals("Password Hidden")) {
+                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                visibilityButton.setContentDescription("Password Visible");
+                visibilityButton.setImageResource(R.drawable.ic_password_visible);
+                toast = Toast.makeText(LoginActivity.this, "Show", Toast.LENGTH_SHORT);
+            } else {
+                password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                visibilityButton.setContentDescription("Password Hidden");
+                visibilityButton.setImageResource(R.drawable.ic_password_not_visible);
+                toast = Toast.makeText(LoginActivity.this, "Hide", Toast.LENGTH_SHORT);
             }
+            toast.show();
+            Handler handler = new Handler();
+            handler.postDelayed(toast::cancel, 500);
+            password.setSelection(password.getText().length());
         });
     }
 }

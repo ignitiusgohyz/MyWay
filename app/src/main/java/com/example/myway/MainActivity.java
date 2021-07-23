77a00619b9b0c5ff3c1bf3abdfd4d6327bd496b9
ignitiusgohyz <ignitiusgohyz@gmail.com>
2021-07-23 23:33:51 +0800

    compass
    (cherry picked from commit 57756eb0da75ce4515a811bf9d8ee62c698feca7)

--- app/src/main/java/com/example/myway/MainActivity.java
+++ app/src/main/java/com/example/myway/MainActivity.java
@@ -230,12 +230,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         resetNorth.setOnClickListener(v -> {
             mapboxMap.resetNorth();
-            mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
-                    new CameraPosition.Builder()
-                            .target(new LatLng(locationComponent.getLastKnownLocation().getLatitude(),
-                                    locationComponent.getLastKnownLocation().getLongitude()))
-                            .zoom(14)
-                            .build()), 4000);
         });
     }
 
